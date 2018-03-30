5662bc5baa60433413ff079bbcc15e0ea1a3cd9d
Neil Seejoor <nvseejoor@gmail.com>
2018-03-30 22:11:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,11 +48,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    a = Student.new(row[0], row[1], row[2])
-    # a.id = row[0]
-    # a.name = row[1]
-    # a.grade = row[2]
-    a
+    Student.new(row[0], row[1], row[2])
   end
 
   def self.find_by_name(name)
