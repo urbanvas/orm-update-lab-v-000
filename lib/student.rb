f16a69ebeef110d02583ff9c7df275ce0ca08e54
Neil Seejoor <nvseejoor@gmail.com>
2018-03-30 21:56:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,16 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(id=nil, name, grade)
-    @id = id 
+    @id = id
     @name = name
     @grade = grade
+  end
+
+  def self.create_table
+
   end
 
 
