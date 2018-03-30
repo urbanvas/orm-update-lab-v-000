6a83b4acac4888550b38a2538f124c94e513fd84
Neil Seejoor <nvseejoor@gmail.com>
2018-03-30 22:04:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,18 @@ class Student
     a
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+          SELECT * FROM students
+          WHERE name = ?
+          LIMIT 1
+          SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
 
 
 end
